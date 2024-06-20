f439635f0a3b06ccda1464c8ed9d0054f88871fc
Matheus Costa da Fonseca <106177144+matheus-costa@users.noreply.github.com>
2024-06-20 20:22:25 -0300

    Delete TCC directory

--- TCC/index.php
+++
@@ -1,6 +0,0 @@
-<p>
-    <link rel="stylesheet" href="style.css">
-    <a href="/View/cadastro_cliente.html">Cadastrar Cliente</a>
-    <a href="/View/cadastro_fornecedor.html">Cadastrar Forecedores</a>
-    <a href="/View/cadastro_proprietario.html">Cadastrar Proprietarios</a>
-</p>
