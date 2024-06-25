a8a9314a09b0acc23ccca05f849519b65a789d77
Matheus Costa da Fonseca <106177144+matheus-costa@users.noreply.github.com>
2024-06-25 18:19:09 -0300

    Add files via upload

---
+++ CICMAP/View/Funcionario/funcionario.php
@@ -0,0 +1,4 @@
+<p>
+    <a href="cadastro_funcionario.html">Cadastrar de funcionarios</a>
+    <a href="/Config/funcionario/lista_funcionario.php">Lista de funcionarios</a>
+</p>
---
+++ CICMAP/View/Produto/produto.php
@@ -0,0 +1,4 @@
+<p>
+    <a href="cadastro_produto.html">Cadastrar de Produtos</a>
+    <a href="/Config/produto/lista_produto.php">Lista de Produtos</a>
+</p>
