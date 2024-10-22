0924d23dfc97dcede76b9ee5ed3c72c60b8bba74
Matheus Costa da Fonseca <106177144+matheus-costa@users.noreply.github.com>
2024-10-22 18:23:05 -0300

    Add files via upload

---
+++ TCC/CICMAP/Model/ModeloPropriedade.php
@@ -0,0 +1,16 @@
+<?php
+    include_once 'ModeloItem.php';
+    include_once 'ModeloPessoa.php';
+
+    class ModeloPropriedade {
+
+        public $id;
+        public $banca;
+        public $pessoa;
+
+        function __construct( $id, ModeloBanca $banca, ModeloPessoa $pessoa) {
+            $this->id = $id;
+            $this->banca = $banca;
+            $this->pessoa = $pessoa;
+        }
+    }
---
+++ TCC/CICMAP/View/VisaoBanca.php
@@ -0,0 +1,63 @@
+<?php
+    include_once 'VisaoLayout.php';
+
+    class VisaoBanca extends VisaoLayout {
+
+        function listarBancas( array $bancas ) {
+            $html = <<<HTML
+                <h1>Bancas</h1>
+                <table border="1">
+            HTML;
+            foreach ( $bancas as $banca ) {
+                $html .= <<<HTML
+                    <tr>
+                        <td>$banca->id</td>
+                        <td>$banca->nome</td>
+                        <td>$banca->numeracao</td>
+                        <td><a href="?acao=removerBanca&id=$banca->id">X</a></td>
+                        <td><a href="?acao=editarBanca&id=$banca->id">Y</a></td>
+                    </tr>
+                HTML;
+            }
+            $html .= <<<HTML
+                </table>
+                <p>
+                    <a href="?acao=cadastrarBanca">Novo</a>
+                </p>
+            HTML;
+            return $html;
+        }
+
+        function cadastrarBancas ( array $banca ) {
+            $html = <<<HTML
+                <h1>Cadastrar Bancas</h1>
+                <form>
+                    <p>
+                        Nome: <br/>
+                        <input type="text" name="nome" autocomplete="off" />
+                    </p>
+                    <p>
+                        Numeracao: <br/>
+                        <input type="text" name="numeracao" autocomplete="off" />
+                    </p>
+                    <p>
+                        <select name="banca">
+                            <option hidden>Selecione</option>
+            HTML;
+            foreach ( $banca as $bancas ) {
+            $html .= <<<HTML
+                            <option value="$->id">$bancas->nome</option>
+            HTML;
+            }
+            $html .= <<<HTML
+                        </select>
+                    </p>
+                    <p>
+                        <button type="submit" formmethod="post" formaction="?acao=salvarBanca">Cadastrar</button>
+                    </p>
+                </form>
+            HTML;
+            return $html;
+        }
+    }
+?>
