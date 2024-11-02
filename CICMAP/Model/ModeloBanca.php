12db51b5fc91e70a1678aabca4e4f3c0135b1a95
Matheus Costa da Fonseca <106177144+matheus-costa@users.noreply.github.com>
2024-11-02 11:31:20 -0300

    Add files via upload

---
+++ CICMAP/Model/ModeloBanca.php
@@ -0,0 +1,15 @@
+<?php
+
+    class ModeloBanca {
+
+        public $id;
+        public $nome;
+        public $numeracao;
+ 
+        function __construct( $id, $nome, $numeracao) {
+            $this->id = $id;
+            $this->nome = $nome;
+            $this->numeracao = $numeracao;
+        }
+    }
+    
