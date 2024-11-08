bc3c1c1c41fd7b887c04c4252b5084651d776cd7
matheus-costa <matheuscostafinsceca@gmail.com>
2024-11-08 00:31:30 -0300

    refactor: refatorando a visão layout, para acessar listar todas as tabelas  banco de dados

--- CICMAP/View/VisaoLayout.php
+++ CICMAP/View/VisaoLayout.php
@@ -14,7 +14,7 @@
                     <a href="?acao=listarProprietarios">Proprietarios</a>
                     <a href="?acao=listarFuncionarios">Funcionario</a>
                     <a href="?acao=listarFornecedores">Fornecedor</a>
-                    <a href="?acao=listarClientes">Clientes</a>
+                    <a href="?acao=listarPessoas">Clientes</a>
                     <a href="?acao=listarVendas">Vendas</a>
                 </p>
         HTML;
