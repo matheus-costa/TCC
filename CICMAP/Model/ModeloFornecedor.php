9758a1247d28f10885d13937548aab8be65c1200
matheus-costa <matheuscostafinsceca@gmail.com>
2024-11-09 16:13:53 -0300

    refactor: modificando a modelo fornecedor  para o modelo ficar condizente com o DB

--- CICMAP/Model/ModeloFornecedor.php
+++ CICMAP/Model/ModeloFornecedor.php
@@ -1,17 +1,11 @@
 <?php
-   include_once 'ModeloPessoa.php';
-
     class ModeloFornecedor {
 
         public $id;
         public $cnpj;
-        public $pessoa;
         
-
-  function __construct( $id, $cnpj, ModeloPessoa $pessoa  ) {
+  function __construct( $id, $cnpj ) {
             $this->id = $id;
             $this->cnpj = $cnpj;
-            $this->pessoa = $pessoa;
-
         }
     }
