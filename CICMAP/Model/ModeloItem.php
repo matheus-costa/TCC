1488ac1e3ea924d56f0d943f19eedffdf42fc4f5
matheus-costa <matheuscostafinsceca@gmail.com>
2024-11-09 16:12:08 -0300

    refactor: modificando a modelo propriedade  para o modelo ficar condizente com o DB

--- CICMAP/Model/ModeloItem.php
+++ CICMAP/Model/ModeloItem.php
@@ -3,22 +3,18 @@
     include_once 'ModeloProduto.php';
     include_once 'ModeloFornecedor.php';
     include_once 'ModeloBanca.php';
-    include_once 'ModeloVenda.php';
-
     class ModeloItem {
 
         public $id;
         public $produto;
         public $banca;
         public $fornecedor;
-        public $venda;   
 
         function __construct( $id, ModeloProduto $produto, ModeloFornecedor $fornecedor
-        ,ModeloBanca $banca, ModeloVenda $venda) {
+        ,ModeloBanca $banca) {
             $this->id = $id;
             $this->produto = $produto;
             $this->banca = $banca;
             $this->fornecedor = $fornecedor;
-            $this->venda = $venda;
         }
     }
