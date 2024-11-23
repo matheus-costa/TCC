afa38b601fa93a9a8f61debfa98b2eef9f7a9cb7
matheus-costa <matheuscostafinsceca@gmail.com>
2024-11-23 19:11:08 -0300

    feat: Adicionando a página cadastro de pessoa, pois ela passará a cadastrar os usuários do sistema

---
+++ CICMAP/View/VisaoPessoa.php
@@ -0,0 +1,114 @@
+<?php
+
+    include_once 'VisaoLayout.php';
+
+    class VisaoPessoa extends VisaoLayout {
+
+        function listarPessoas( array $pessoas ) {
+            $html = <<<HTML
+                <h1>Pessoas</h1>
+                <table border="1">
+                <tr>
+                    <th> Nome da Pessoa     </th>
+                    <th> CPF da Pessoa     </th>
+                    <th> RG  da Pessoa		  </th>
+                    <th> E-mail  da Pessoa	  </th>
+                    <th> Telefone  da Pessoa </th>
+                    <th> Endereço da Pessoa  </th>
+                    <th> Excluir  Pessoa    </th>
+                </tr>
+            HTML;
+            foreach ( $pessoas as $pessoa ) {
+
+                $html .= <<<HTML
+                    <tr>
+                        <td>$pessoa->nome</td>
+                        <td>$pessoa->cpf</td>
+                        <td>$pessoa->rg</td>
+                        <td>$pessoa->email</td>
+                        <td>$pessoa->telefone</td>
+                        <td>$pessoa->endereco</td>
+                        <td><a href="?acao=removerPessoa&id=$pessoa->id">X</a></td>
+                    </tr>
+                HTML;
+            }
+            $html .= <<<HTML
+                </table>
+                <p>
+                    <a href="?acao=cadastrarPessoas">Novo</a>
+                </p>
+            HTML;
+            return $html;
+        }
+
+        function cadastrarPessoas($pessoa ,$propriedade, $trabalho) {
+            $html = <<<HTML
+                <h1>Cadastrar Pessoas</h1>
+
+                <form>
+                    <p>
+                        Nome  <br/>
+                        <input type="text" name="nome" autocomplete="off" />
+                    </p>
+                    <p>
+                        CPF <br/>
+                        <input type="text" name="cpf" autocomplete="off" />
+                    </p>
+                    <p>
+                        RG <br/>
+                        <input type="text" name="rg" autocomplete="off" />
+                    </p>
+                    <p>
+                        E-MAIL <br/>
+                        <input type="text" name="email" autocomplete="off" />
+                    </p>
+                    <p>
+                        TELEFONE <br/>
+                        <input type="text" name="telefone" autocomplete="off" />
+                    </p>
+                    <p>
+                        ENDERECO <br/>
+                        <input type="text" name="endereco" autocomplete="off" />
+                    </p>
+                    
+                    <p>
+                        <select name ="propriedade">
+                            <option hidden>Selecionar Banca</option>
+                    </p>        
+            HTML;       
+            foreach ( $propriedade as $propriedades ) {
+                $banca = $propriedades->banca;
+            $html .= <<<HTML
+                            <option value="$propriedades->id">$banca->nome</option>
+            HTML;
+                            }
+            $html .= <<<HTML
+                        </select>
+                    </p>
+
+                    <p>
+                        <select name="trabalho">
+                            <option hidden>Selecionar Trabalho</option>
+            HTML;
+            foreach ( $trabalho as $trabalhos ) {
+                $banca = $trabalhos->banca;
+            $html .= <<<HTML
+                            <option value="$trabalhos->id">$banca->nome</option>
+            HTML;
+            }
+            $html .= <<<HTML
+                        </select>
+                    </p>
+            HTML;
+           
+            $html .= <<<HTML
+                    </p>
+
+                    <p>
+                        <button type="submit" formmethod="post" formaction="?acao=salvarPessoas">Cadastrar</button>
+                    </p>
+                </form>
+            HTML;
+            return $html;
+        }
+    }
