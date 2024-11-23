95af851d7bb21be2d9010396d255744deb77da41
matheus-costa <matheuscostafinsceca@gmail.com>
2024-11-23 19:16:38 -0300

    refactor:modificando a visão layout pois alguns links não serão mais utilizados no projeto

--- CICMAP/View/VisaoLayout.php
+++ CICMAP/View/VisaoLayout.php
@@ -11,10 +11,8 @@
                 <p>
                     <a href="?acao=listarProdutos">Produtos</a>
                     <a href="?acao=listarBancas">Bancas</a>
-                    <a href="?acao=listarProprietarios">Proprietarios</a>
-                    <a href="?acao=listarFuncionarios">Funcionario</a>
                     <a href="?acao=listarFornecedores">Fornecedor</a>
-                    <a href="?acao=listarPessoas">Clientes</a>
+                    <a href="?acao=listarPessoas">Pesssoas</a>
                     <a href="?acao=listarVendas">Vendas</a>
                 </p>
         HTML;
