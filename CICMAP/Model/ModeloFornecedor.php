cbe4705ac1b8d3a627dea92bbd4ab8c3d9f9c870
matheus-costa <matheuscostafinsceca@gmail.com>
2024-11-30 02:29:26 -0300

    fix: refatorando o código para resolver outro bug

--- CICMAP/Model/ModeloFornecedor.php
+++ CICMAP/Model/ModeloFornecedor.php
@@ -1,11 +1,16 @@
 <?php
+
+include_once "ModeloPessoa.php";
     class ModeloFornecedor {
 
         public $id;
         public $cnpj;
+        public $pessoa;
+
         
-  function __construct( $id, $cnpj ) {
+  function __construct( $id, $cnpj, ModeloPessoa $pessoa ) {
             $this->id = $id;
             $this->cnpj = $cnpj;
+            $this->pessoa = $pessoa;
         }
     }
