ab6e2a772765b2c5450d87078d0a7f45f259191b
matheus-costa <matheuscostafinsceca@gmail.com>
2024-11-30 02:33:39 -0300

    refactor: refatorando o medelo venda para a listagem

--- CICMAP/Model/ModeloVenda.php
+++ CICMAP/Model/ModeloVenda.php
@@ -5,11 +5,13 @@
     class ModeloVenda {
 
         public $id;
+        public $dataHora;
         public $pessoa;
         public $item;
 
-        function __construct( $id, ModeloPessoa $pessoa, ModeloItem $item) {
+        function __construct( $id, $dataHora, ModeloPessoa $pessoa, ModeloItem $item) {
             $this->id = $id;
+            $this->dataHora= $dataHora;
             $this->pessoa = $pessoa;
             $this->item = $item;
         }
