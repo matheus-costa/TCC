cc225bc6312d1f07644d0fe87ef9c3d9db6e560d
matheus-costa <matheuscostafinsceca@gmail.com>
2025-01-16 18:31:13 -0300

    feat: adicionando funcionalidades

--- CICMAP/Model/ModeloItem.php
+++ CICMAP/Model/ModeloItem.php
@@ -6,13 +6,17 @@
     class ModeloItem {
 
         public $id;
+        public $data_compra;
+        public $quantidade;
         public $produto;
         public $banca;
         public $fornecedor;
 
-        function __construct( $id, ModeloProduto $produto, ModeloFornecedor $fornecedor
+        function __construct( $id, $data_compra, $quantidade, ModeloProduto $produto, ModeloFornecedor $fornecedor
         ,ModeloBanca $banca) {
             $this->id = $id;
+            $this->data_compra = $data_compra;
+            $this->quantidade = $quantidade;
             $this->produto = $produto;
             $this->banca = $banca;
             $this->fornecedor = $fornecedor;
